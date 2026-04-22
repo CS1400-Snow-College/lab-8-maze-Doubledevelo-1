81a8eab92fdc9a8d652242e07db8b7c68d6b6213
Doubledevelo <massimolj@outlook.com>
2026-04-21 22:50:14 -0600

    initialised code

---
+++ Maze.cs
@@ -0,0 +1,4 @@
+﻿// Massimo Lovjoy April 21st 2026, Maze
+
+Console.WriteLine("Welcome to the maze game! You will have to navigte the dark and dangerous maze to find the *! Use the arrow keys to move and escape to quit.");
+
